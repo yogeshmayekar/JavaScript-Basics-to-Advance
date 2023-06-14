8a2e971de01f124bd8c5d2d66209adf88cac30f1
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-14 21:14:41 +0530

    String Concatination

---
+++ Concatination.js
@@ -0,0 +1,7 @@
+// String Concatination 
+let firstName = "Yogesh";
+let fatherName = "Mangesh";
+let lastName = "Mayekar";
+
+let fullName = firstName +" " + fatherName + " " + lastName;
+console.log("Full Nmae is:" , fullName);
