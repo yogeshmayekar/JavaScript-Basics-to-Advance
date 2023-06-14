18e271ab164c1759d0cea3e77c49dc785c6307d4
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-14 19:22:48 +0530

    First hello world program and variables in javaScript

---
+++ HelloWorld.js
@@ -0,0 +1,33 @@
+// declar a veriable to srore message
+var message = "Hello World";
+// to print on console 
+console.log(message);
+
+// to change the value of the variable 
+message = "Namaskara From Yogesh";
+
+console.log(message);
+
+// declaring the variable with var keyworld
+var firstName = "Suresh";
+var firstName = "Yogesh";
+console.log(firstName);
+
+
+// declaring the variable with let keyworld
+
+// let names = "Yogesh";
+// let names = "Rohan";
+// console.log(names);     we cannot declear twice the same variable with let keyword
+
+let names ="Yogesh";
+names ="Rohan";
+console.log(names);
+
+// declaring constant 
+const pi = 3.14;
+console.log(pi);
+
+// cannot change the value of constant
+// pi = 3.99;
+// console.log(pi);
