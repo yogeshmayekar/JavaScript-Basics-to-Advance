924938c00b4185946f8eea2f334584e4195d1205
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-15 13:28:33 +0530

    if else conditions

---
+++ ifElseConditions.js
@@ -0,0 +1,29 @@
+// if Else conditions 
+let age =19;
+if(age >=18){
+    console.log("Your are adult");
+}
+
+// to check the number is even or odd 
+let number = 15;
+if(number%2 === 0){
+    console.log("Given number is even");
+}else{
+    console.log("Given number is odd");
+}
+
+// if else example 2 
+let tempInDegree = 23;
+if(tempInDegree < 0){
+console.log("extremely cold outside");
+}else if(tempInDegree < 16){
+    console.log("It is cold outside");
+}else if(tempInDegree < 25){
+    console.log("wether is ok");
+}else if(tempInDegree < 35){
+    console.log("lets go for swim");
+}else if(tempInDegree < 45){
+    console.log("turn on AC");
+}else{
+    console.log("its to hot outside");
+}
