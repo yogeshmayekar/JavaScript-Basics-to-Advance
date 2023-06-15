8837b3d5124803d961989abfb6d7966c4106d3ed
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-15 14:09:16 +0530

    For loop example

---
+++ ForLoop.js
@@ -0,0 +1,11 @@
+// for loop 
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+
+// sum of n natural number using for loop 
+let sum = 0;
+for(let i=0; i<=15; i++){
+    sum += i;
+}
+console.log("sum is:", sum);
