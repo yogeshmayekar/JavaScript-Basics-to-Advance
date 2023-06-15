98801139ebbb08ec20d0ae46597dff2475670a35
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-15 14:00:29 +0530

    While loop Program

---
+++ WhileLoop.js
@@ -0,0 +1,20 @@
+// While loop 
+let i = 0;
+
+while(i<10){
+    console.log(i);
+    i++;
+}
+console.log(`current value of the i is ${i}`);
+
+
+// sum of the n natural numbers 
+let num= 0;
+let totel = 0;
+
+while(num <= 15){
+    totel += num;
+    num ++;
+}
+
+console.log("sum is:", totel);
