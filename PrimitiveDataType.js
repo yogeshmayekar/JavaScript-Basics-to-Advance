d880788fbf55670c96386939c3d5f42499ec8462
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-16 20:28:15 +0530

    Primitive data type Memory relation example

---
+++ PrimitiveDataType.js
@@ -0,0 +1,19 @@
+// Primitive data type 
+let number1 = 34;
+let number2 = number1;
+console.log("value of the number 1 is:", number1);
+console.log("value of the number 2 is:", number2);
+
+number1 = 45;
+console.log("value of the number 1 after update is:", number1);
+console.log("value of the number 2 after update is:", number2);
+
+// another example 
+let str1 = "Yogesh";
+let str2 = str1;
+console.log("Value of string 1 is:", str1); 
+console.log("Value of string 2 is:", str2); 
+
+str1 = "Yogesh" + "Mayekar";
+console.log("Value of string 1 after update is:", str1); 
+console.log("Value of string 2 after update is:", str2); 
