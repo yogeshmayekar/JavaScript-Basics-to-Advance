38e6b87410b4fd5dcc8d041e8917b06985880bdd
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-16 20:42:15 +0530

    Reference data type example

---
+++ RefrenceDataType.js
@@ -0,0 +1,9 @@
+// refrence data type 
+let arr1 = [1, 2, 3, 4];
+let arr2 = arr1;
+console.log("value of the array 1 is:", arr1);
+console.log("value of the array 2 is:", arr2);
+
+arr1.push(5);
+console.log("value of the array 1 after update is:", arr1);
+console.log("value of the array 2 after update is:", arr2);
