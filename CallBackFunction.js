4c0928590fe0a220fc64b52bdfc46823237d924e
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-19 20:07:45 +0530

    function returning function

--- CallBackFunction.js
+++ CallBackFunction.js
@@ -9,3 +9,15 @@ function myFunction(cb){
 }
 
 myFunction(myFunction2);
+
+// function returning function 
+function myFunc(){
+    function hello(){
+        console.log("hello world");
+    }
+    return hello;
+}
+
+const ans = myFunc();
+console.log(ans);
+ans();  // function calling
