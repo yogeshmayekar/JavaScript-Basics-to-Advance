2ddfd8f00b9aa1bd05d38120c17f21eb7731bc66
yogesh_mayekar <112848238+yogeshmayekar@users.noreply.github.com>
2023-06-23 20:44:25 +0530

    Usage of call, apply, bind ,ethon in objects

--- Methods.js
+++ Methods.js
@@ -29,3 +29,32 @@ const client4 = {
 
 client3.details();
 client1.details();
+
+// call apply and bind 
+
+function about(industry, exp){
+    console.log(`my name is ${this.firstName} and my age is ${this.age}`, industry, exp)
+}
+
+// call 
+const user1 = {
+    firstName: "Sanchana",
+    age : 24,
+}
+
+const user2 = {
+    firstName: "yogita",
+    age:23,
+}
+
+// user1.about.call(user2)
+// user1.about.call()
+about.call(user1,"finance", "10/10")  //to pass the argument
+
+// apply 
+about.apply(user1, ["civil","10/10"])
+
+//blind 
+// this method returns function 
+const reFunction = about.bind(user2, "fintech", "9/10");
+reFunction();
